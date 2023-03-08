919722928c5e53dcb8d3c54463ee794c2b30a544
MineSai <40472691+MineSai@users.noreply.github.com>
2023-03-08 09:40:35 +0100

    Update animation.py

--- animation.py
+++ animation.py
@@ -4,28 +4,37 @@ import pygame
 
 class animation:
     def __init__(self, nb):
-        self.state = 0  # Etat du joueur (0: Ne bouge pas, 1: est en train de marcher)
+        self.state = 0  # Etat du joueur (0: Ne bouge pas, 1: est en train de marcher, 2: Tappe le joueur)
         self.left_direction = False  # Va à gauche ou à droite
         self.frame_rate = 4  # L'animation du joueur change toutes les 4 frames
         self.current_frame = 0
-        spritesheet = SpriteSheet('assets/player'+str(nb)+'_idle.png')
+        spritesheet = SpriteSheet('assets/player'+str(nb)+'_idle.png') # Joueur immobile
         self.player_idle_images = spritesheet.images(1, 6)  # Découpe les images du joueur
-        spritesheet = SpriteSheet('assets/player'+str(nb)+'_walk.png')
+        spritesheet = SpriteSheet('assets/player' + str(nb) + '_walk.png') # Joueur qui marche
         self.player_walk_images = spritesheet.images(1, 6)  # Découpe les images du joueur
+        spritesheet = SpriteSheet('assets/player' + str(nb) + '_hit.png')  # Joueur qui tappe
+        self.player_hit_images = spritesheet.images(1, 9)  # Découpe les images du joueur
 
         # Redimensionner chaque image individuellement
         for i in range(len(self.player_idle_images)):
             self.player_idle_images[i] = pygame.transform.scale(self.player_idle_images[i], (60,120))
         for i in range(len(self.player_walk_images)):
             self.player_walk_images[i] = pygame.transform.scale(self.player_walk_images[i], (60, 120))
+        for i in range(len(self.player_hit_images)):
+            self.player_hit_images[i] = pygame.transform.scale(self.player_hit_images[i], (60, 120))
 
         self.player_idle_index = 0
         self.player_walk_index = 0
+        self.player_hit_index = 0
 
     def get_current_image(self):
         current_image = self.player_idle_images[self.player_idle_index]
         if self.state == 1:
             current_image = self.player_walk_images[self.player_walk_index]
+            if self.left_direction:
+                current_image = pygame.transform.flip(current_image, True, False)  # Retourne l'image si le joueur va à gauche
+        elif self.state == 2:
+            current_image = self.player_hit_images[self.player_hit_index]
             if self.left_direction:
                 current_image = pygame.transform.flip(current_image, True, False)  # Retourne l'image si le joueur va à gauche
         return current_image
@@ -38,6 +47,10 @@ class animation:
                 self.player_idle_index = self.update_index(self.player_idle_index, 6)
             elif self.state == 1:
                 self.player_walk_index = self.update_index(self.player_walk_index, 6)
+        if self.state == 2:
+            self.player_hit_index = self.update_index(self.player_hit_index, 9)
+            if self.player_hit_index == 0:
+                self.state = 0
 
     def update_index(self, index, indexMax):
         index = index+1
