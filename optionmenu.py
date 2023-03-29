8794de2666b919fefee5ba8d0549a6abdfb844ec
MineSai <40472691+MineSai@users.noreply.github.com>
2023-03-29 08:43:34 +0200

    Add files via upload

---
+++ optionmenu.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Optionmenu:
+
+    def __init__(self):
+        self.isOpened = False
+
+    def update(self, screen):
+
+        return
+
+    def open(self):
+        self.isOpened = True
+
+    def close(self):
+        self.isOpened = False
